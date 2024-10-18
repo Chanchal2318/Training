eb27a2b407e6189a03c7b679baca84e7c7bd8552
Chanchal2318 <133849770+Chanchal2318@users.noreply.github.com>
2024-10-18 06:20:21 +0000

    Props

---
+++ Notes/18-oct-2024.js
@@ -0,0 +1,19 @@
+// Props
+
+// 1. It is used when we have to send data  along with component call
+
+// for ex:- <App data={name}/> . It is received in props in a component
+
+// 2. We can send multiple values during a function call , but they all can received in a single parameter called 'props' and all values can stay together in a form of an object.
+
+// 3. Props are immutable -> No changes are allowed
+
+// 4. We have to install a dependancy called 'props-Type' to use the feature of props
+
+// 5. All object are send into {} curly brackets except string as they are send into single-quote '' or double-quote "".
+
+// 6. We can also set validation in a component to restrict the datatype of the particular props.
+
+// 7. We can set defaultProps for the situation in which we don't receive any prop then we will use the defaultProps .
+
+// 8. If we have both default as well as prop send by the user then we always give priority to the props send by the user.
